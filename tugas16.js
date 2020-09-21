c8e79e0255db5d392c9f096300cbbd2356f77418
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 10:29:45 +0700

    Add files via upload

---
+++ tugas16.js
@@ -0,0 +1,11 @@
+function panggilMin(nilai1, nilai2, nilai3, nilai4, nilai5, nilai6, nilai7, nilai8, nilai9, nilai10, nilai11, nilai12, nilai13){
+	return Math.min(nilai1, nilai2, nilai3, nilai4, nilai5, nilai6, nilai7, nilai8, nilai9, nilai10, nilai11, nilai12, nilai13)
+}
+
+function panggilMax(nilai1, nilai2, nilai3, nilai4, nilai5, nilai6, nilai7, nilai8, nilai9, nilai10, nilai11, nilai12, nilai13){
+	return Math.max(nilai1, nilai2, nilai3, nilai4, nilai5, nilai6, nilai7, nilai8, nilai9, nilai10, nilai11, nilai12, nilai13)
+}
+
+console.log(panggilMin(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10))
+
+console.log(panggilMax(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5))
